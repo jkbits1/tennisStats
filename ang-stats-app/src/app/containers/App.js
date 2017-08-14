70860fdff47a78ca9b8c41adaf803ac738547ab3
jkbits1 <jkbits1@users.noreply.github.com>
2017-08-14 01:57:33 +0100

    basic player display

--- ang-stats-app/src/app/containers/App.js
+++ ang-stats-app/src/app/containers/App.js
@@ -1,10 +1,17 @@
 import {SHOW_ALL} from '../constants/TodoFilters';
-import {initialTodo} from '../todos/todos';
+// import {initialTodo} from '../todos/todos';
 import {initialPlayer} from '../players/players';
 
 class AppController {
   constructor() {
-    this.todos = [initialTodo];
+    this.todos =
+      // [initialTodo];
+      // JSON.parse(
+        angular.fromJson(
+          [{Rank: '10', Name: 'John Kelly', Won: '1', Played: '9', Sets: '2 - 9', Games: '31 - 67'},
+            {Rank: '11', Name: 'Mike Milfull', Won: '1', Played: '9', Sets: '2 - 10', Games: '40 - 65'},
+            {Rank: '12', Name: 'John Trotter', Won: '0', Played: '3', Sets: '1 - 3', Games: '10 - 22'}
+          ]);
     this.players = [initialPlayer];
     this.filter = SHOW_ALL;
   }
