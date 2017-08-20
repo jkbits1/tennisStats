55108261b1aba65cd2f2411867708dd707f5ac26
jkbits1 <jkbits1@users.noreply.github.com>
2017-08-21 00:45:48 +0100

    refactor controllers

--- ang-stats-app/src/app/components/TablePlayers.js
+++ ang-stats-app/src/app/components/TablePlayers.js
@@ -5,76 +5,65 @@
 // https://stackoverflow.com/questions/27448789/angularjs-nested-directives-are-inserted-outside-their-supposed-parent-element
 // https://stackoverflow.com/questions/15362868/adding-rows-with-ng-repeat-and-nested-loop
 
+function TablePlayersController ($scope) {
+  console.log('table player scope', $scope);
+
+  $scope.sortPlayersByName = function () {
+    console.log('button clicked');
+    console.log('players count', $scope.players.length);
+
+    $scope.players.sort((a, b) => {
+      const nameA = a.Name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.Name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0;
+    });
+  };
+
+  $scope.sortPlayersByRank = function () {
+    console.log('button clicked');
+    console.log('players count', $scope.players.length);
+
+    $scope.players.sort((a, b) => {
+      return a.Rank - b.Rank;
+    });
+  };
+
+  $scope.sortPlayersByWins = function () {
+    console.log('button clicked');
+    console.log('players count', $scope.players.length);
+
+    $scope.players.sort((a, b) => {
+      return b.Won - a.Won;
+    });
+  };
+
+  $scope.sortPlayersByParticipation = function () {
+    console.log('button clicked');
+    console.log('players count', $scope.players.length);
+
+    $scope.players.sort((a, b) => {
+      return b.Played - a.Played;
+    });
+  };
+}
+
+TablePlayersController.$inject = ['$scope'];
+
 function TablePlayersDirective () {
     return {
         restrict: 'E',
         templateUrl: './app/components/TablePlayers.html',
-        // template: [
-        //     '<table class="table table-player" ng-show="players.length">',
-        //         // '<thead>',
-        //         //     '<tr>',
-        //         //         '<th class="query-name">Name</th>',
-        //         //         '<th class="query-status">Status</th>',
-        //         //     '</tr>',
-        //         // '</thead>',
-        //         '<tbody>',
-        //             '<tr row-player ng-repeat="player in players track by $index"',
-        //                      'player="player">',
-        //             '</tr>',
-        //         '</tbody>',
-        //     '</table>'
-        // ].join(''),
         scope: {
             players: '='
         },
-        controller: ['$scope', function($scope) {
-          console.log('table player scope', $scope);
-
-          $scope.sortPlayersByName = function () {
-            console.log('button clicked');
-            console.log('players count', $scope.players.length);
-
-            $scope.players.sort((a, b) => {
-              const nameA = a.Name.toUpperCase(); // ignore upper and lowercase
-              const nameB = b.Name.toUpperCase(); // ignore upper and lowercase
-              if (nameA < nameB) {
-                return -1;
-              }
-              if (nameA > nameB) {
-                return 1;
-              }
-
-              return 0;
-            });
-          };
-
-          $scope.sortPlayersByRank = function () {
-            console.log('button clicked');
-            console.log('players count', $scope.players.length);
-
-            $scope.players.sort((a, b) => {
-              return a.Rank - b.Rank;
-            });
-          };
-
-          $scope.sortPlayersByWins = function () {
-            console.log('button clicked');
-            console.log('players count', $scope.players.length);
-
-            $scope.players.sort((a, b) => {
-              return b.Won - a.Won;
-            });
-          };
-
-          $scope.sortPlayersByParticipation = function () {
-            console.log('button clicked');
-            console.log('players count', $scope.players.length);
-
-            $scope.players.sort((a, b) => {
-              return b.Played - a.Played;
-            });
-          };
-        }],
+        controller: TablePlayersController,
         link: function(scope, element) {
         }
     };
