ec7560968be1a4016cadc85f856cd09c5538a078
jkbits1 <jkbits1@users.noreply.github.com>
2017-09-26 21:53:05 +0100

    add cors support to server

--- exp-stats/app.js
+++ exp-stats/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// https://stackoverflow.com/questions/7067966/how-to-allow-cors
+var cors = require('cors');
+
 var index = require('./routes/index');
 // var users = require('./routes/users');
 var playersRouter = require('./routes/players');
@@ -74,6 +77,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(cors());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
